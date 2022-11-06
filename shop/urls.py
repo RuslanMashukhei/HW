ca10ada01d99b8d304dfac3c802504beaba85d02
RuslanMashukhei <rmasuhej@gmail.com>
2022-11-06 13:33:06 +0600

    second hw

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import greetings, list_item, detail_item
+from .views import greetings, ItemListView, DetailItemView
 urlpatterns = [
     path('shop/greetings', greetings, name="greetings"),
-    path('shop/', list_item, name="list_item"),
-    path('shop/<int:item_id>/', detail_item, name="detail_item")
+    # path('shop/', list_item, name="list_item"),
+    path('shop/', ItemListView.as_view(), name="list_item"),
+    # path('shop/<int:item_id>/', detail_item, name="detail_item")
+    path('shop/<int:pk>/', DetailItemView.as_view(), name="detail_item"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Item
+from django.views import generic
 
 def greetings(request):
     return HttpResponse("Добро пожаловать в наш магазин!")
-def list_item(request,item_id):
-    # return HttpResponse(f"Вы находитесь на странице для просмотра товаров {item_id} ")
-    items = Item.objects.all()
-    context = {
-        'items': items,
-    }
-    return render(request, 'list_item.html' , context )
+# def list_item(request,item_id):
+#     # return HttpResponse(f"Вы находитесь на странице для просмотра товаров {item_id} ")
+#     items = Item.objects.all()
+#     context = {
+#         'items': items,
+#     }
+#     return render(request, 'list_item.html' , context )
 
-def detail_item(request, item_id):
-    #item = Item.objects.get(id=item_id)
-    item = get.object_or_404(Item, id=item_id)
-    context = {
-        'item': item,
-    }
-    return render(request, 'detail_item.html', context)
+class ItemListView(generic.ListView):
+    model = Item
+    template_name = 'list_item.html'
+    context_object_name = 'items'
+
+
+# def detail_item(request, item_id):
+#     #item = Item.objects.get(id=item_id)
+#     item = get.object_or_404(Item, id=item_id)
+#     context = {
+#         'item': item,
+#     }
+#     return render(request, 'detail_item.html', context)
+class DetailItemView(generic.DetailView):
+    model = Item
+    template_name = 'detail_item.html'
+    context_object_name = 'item.purchase_set_all'
